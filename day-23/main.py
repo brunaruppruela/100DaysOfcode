749b12c87473b2a87320925528e4da04b44c9d61
Bruna Rupp <brunarupp@gmail.com>
2021-02-04 13:05:58 -0300

    adicionando novos dias

--- day-23/main.py
+++ day-23/main.py
@@ -4,11 +4,20 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+turtle = Player()
+cars = CarManager()
+
+screen.listen()
+screen.onkey(turtle.move, "Up")
 
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    
