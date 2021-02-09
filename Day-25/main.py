a7f316f3b24e84736203f7ade4179398b81d061b
Bruna Rupp <brunarupp@gmail.com>
2021-02-09 19:47:14 -0300

    atualizando os dias

--- Day-25/main.py
+++ Day-25/main.py
@@ -7,6 +7,7 @@ data = pandas.read_csv("./Day-25/squirrel.csv")
 #print(data["Primary Fur Color"]) 
 
 #gray = data[data["Primary Fur Color"] == "Gray"]
+#capturando o tamanho de cada cor de esquilo do dataframe e contando a quantidade de cada
 gray_count = len(data[data["Primary Fur Color"] == "Gray"])
 red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
 black_count = len(data[data["Primary Fur Color"] == "Black"])
@@ -14,12 +15,13 @@ print(gray_count)
 print(red_count)
 print(black_count)
 
+#criando um novo arquivo e inserindo os novos dados de cores captuados 
 data_dict = {
-    
+    """as chaves serao as colunas do novo CSV, os valores serao buscado das variaveis de armazanemanto"""
     "Fur Color": ["Gray", "Cinnamon", "Black"],
     "Count": [gray_count, red_count, black_count]
 
 }
-
+#utilizando a funcao DataFrame para transformar o o dicionario data_dict em um novo csv color_squirrel.csv
 data = pandas.DataFrame(data_dict)
 data.to_csv("color_squirrel.csv")
---
+++ Day-25/project_day/main.py
@@ -0,0 +1,34 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+guessed_states = []
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+turtle.shape(image)
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Corect",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        #trazer a linha que valida que o estado é igual a resposta
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
