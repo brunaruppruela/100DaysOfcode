6309d6c69372db144e1aeb5dd2a5db3049f756bd
Bruna Rupp <brunarupp@gmail.com>
2021-03-10 19:06:42 -0300

    dia 34

---
+++ Day-33/exercicio.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+MY_LAT = -20.336840
+MY_LONG = -40.291931
+
+response = requests.get("https://api.sunrise-sunset.org/json", params = parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
